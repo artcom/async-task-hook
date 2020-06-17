9a986aa75f21ad952e825e37c1823fc41dd91fa9
Mirko Geissler <mirko.geissler@artcom.de>
2020-06-17 15:22:15 +0200

    Cleanup tests

--- test/useAsyncTask.test.js
+++ test/useAsyncTask.test.js
@@ -6,7 +6,7 @@ const FINAL_RESULT = "FINAL_RESULT"
 const VERY_FINAL_RESULT = "VERY_FINAL_RESULT"
 
 describe("useAsyncTask", () => {
-  it("should finish task", async () => {
+  it("should finish task", () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -15,17 +15,16 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
-    await act(() => {
-      render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
-      return delay(20)
-    })
+    render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
+
+    await delay(20)
 
     expect(results.length).toBe(2)
     expect(results[0]).toEqual({ status: RUNNING })
     expect(results[1]).toEqual({ status: FINISHED, result: FINAL_RESULT })
-  })
+  }))
 
-  it("should cancel task", async () => {
+  it("should cancel task", async () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -34,18 +33,16 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
-    await act(() => {
-      const { unmount } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
-      unmount()
+    const { unmount } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
+    unmount()
 
-      return delay(20)
-    })
+    await delay(20)
 
     expect(results.length).toBe(1)
     expect(results[0]).toEqual({ status: RUNNING })
-  })
+  }))
 
-  it("should not re-run task on rerender", async () => {
+  it("should not re-run task on rerender", async () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -54,24 +51,21 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
+    const task = () => delay(10).then(() => FINAL_RESULT)
 
-    await act(async () => {
-      const task = () => delay(10).then(() => FINAL_RESULT)
+    const { rerender } = render(<Component task={ task } />)
+    await delay(20)
 
-      const { rerender } = render(<Component task={ task } />)
-      await delay(20)
+    rerender(<Component task={ task } />)
+    await delay(20)
 
-      rerender(<Component task={ task } />)
-      await delay(20)
+    expect(results.length).toBe(3)
+    expect(results[0]).toEqual({ status: RUNNING })
+    expect(results[1]).toEqual({ status: FINISHED, result: FINAL_RESULT })
+    expect(results[2]).toEqual({ status: FINISHED, result: FINAL_RESULT })
+  }))
 
-      expect(results.length).toBe(3)
-      expect(results[0]).toEqual({ status: RUNNING })
-      expect(results[1]).toEqual({ status: FINISHED, result: FINAL_RESULT })
-      expect(results[2]).toEqual({ status: FINISHED, result: FINAL_RESULT })
-    })
-  })
-
-  it("should run another task after first task", async () => {
+  it("should run another task after first task", async () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -80,22 +74,20 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
-    await act(async () => {
-      const { rerender } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
-      await delay(20)
+    const { rerender } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
+    await delay(20)
 
-      rerender(<Component task={ () => delay(10).then(() => VERY_FINAL_RESULT) } />)
-      await delay(20)
+    rerender(<Component task={ () => delay(10).then(() => VERY_FINAL_RESULT) } />)
+    await delay(20)
 
-      expect(results.length).toBe(4)
-      expect(results[0]).toEqual({ status: RUNNING })
-      expect(results[1]).toEqual({ status: FINISHED, result: FINAL_RESULT })
-      expect(results[2]).toEqual({ status: RUNNING })
-      expect(results[3]).toEqual({ status: FINISHED, result: VERY_FINAL_RESULT })
-    })
-  })
+    expect(results.length).toBe(4)
+    expect(results[0]).toEqual({ status: RUNNING })
+    expect(results[1]).toEqual({ status: FINISHED, result: FINAL_RESULT })
+    expect(results[2]).toEqual({ status: RUNNING })
+    expect(results[3]).toEqual({ status: FINISHED, result: VERY_FINAL_RESULT })
+  }))
 
-  it("should run another task after first task cancelled", async () => {
+  it("should run another task after first task cancelled", async () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -104,20 +96,19 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
-    await act(async () => {
-      const { rerender } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
+    const { rerender } = render(<Component task={ () => delay(10).then(() => FINAL_RESULT) } />)
 
-      rerender(<Component task={ () => delay(10).then(() => VERY_FINAL_RESULT) } />)
-      await delay(20)
+    rerender(<Component task={ () => delay(10).then(() => VERY_FINAL_RESULT) } />)
 
-      expect(results.length).toBe(3)
-      expect(results[0]).toEqual({ status: RUNNING })
-      expect(results[1]).toEqual({ status: RUNNING })
-      expect(results[2]).toEqual({ status: FINISHED, result: VERY_FINAL_RESULT })
-    })
-  })
+    await delay(20)
 
-  it("should return error on error", async () => {
+    expect(results.length).toBe(3)
+    expect(results[0]).toEqual({ status: RUNNING })
+    expect(results[1]).toEqual({ status: RUNNING })
+    expect(results[2]).toEqual({ status: FINISHED, result: VERY_FINAL_RESULT })
+  }))
+
+  it("should return error on error", async () => act(async () => {
     const results = []
 
     const Component = ({ task }) => {
@@ -126,18 +117,16 @@ describe("useAsyncTask", () => {
       return <></>
     }
 
-    await act(async () => {
-      render(<Component
-        task={ () => delay(10).then(() => { throw new Error("This is an error") }) } />
-      )
+    render(<Component
+      task={ () => delay(10).then(() => { throw new Error("This is an error") }) } />
+    )
 
-      await delay(20)
+    await delay(20)
 
-      expect(results.length).toBe(2)
-      expect(results[0]).toEqual({ status: RUNNING })
-      expect(results[1]).toEqual({ status: ERROR, error: new Error("This is an error") })
-    })
-  })
+    expect(results.length).toBe(2)
+    expect(results[0]).toEqual({ status: RUNNING })
+    expect(results[1]).toEqual({ status: ERROR, error: new Error("This is an error") })
+  }))
 })
 
 function delay(time) {
